3ddf19d1b6f1db8c57c9eddc0ee7b4f0fa423d99
HungDPM215465 <hung_dpm215465@student.agu.edu.vn>
2025-11-02 21:09:50 +0700

    Add files via upload

---
+++ Add Parameter/AFTER/AFTER/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AddParameter_After
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+
+            // Có thể chỉ định ngày
+            customer.GetContact(new DateTime(2025, 10, 20));
+        }
+    }
+
+    class Customer
+    {
+        public void GetContact(DateTime date)
+        {
+            Console.WriteLine($"Contact information as of {date.ToShortDateString()}");
+        }
+    }
+}
+
---
+++ Add Parameter/BEFORE/BEFORE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AddParameter_Before
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.GetContact(); // Không thể chỉ định ngày
+        }
+    }
+
+    class Customer
+    {
+        public void GetContact()
+        {
+            Console.WriteLine("Contact information as of today.");
+        }
+    }
+}
+
