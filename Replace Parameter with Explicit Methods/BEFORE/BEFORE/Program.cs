602d9d2324a36772b34347f9f29e34252de04f4c
HungDPM215465 <hung_dpm215465@student.agu.edu.vn>
2025-11-02 21:32:56 +0700

    Add files via upload

---
+++ Replace Parameter with Explicit Methods/BEFORE/BEFORE/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ReplaceParameterWithExplicitMethods_Before
+{
+    class Rectangle
+    {
+        private int height;
+        private int width;
+
+        public void SetValue(string name, int value)
+        {
+            if (name.Equals("height"))
+            {
+                height = value;
+                return;
+            }
+
+            if (name.Equals("width"))
+            {
+                width = value;
+                return;
+            }
+
+            Console.WriteLine("Invalid parameter name!");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Height = {height}, Width = {width}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+
+            rect.SetValue("height", 10);
+            rect.SetValue("width", 20);
+
+            rect.ShowInfo();
+        }
+    }
+}
